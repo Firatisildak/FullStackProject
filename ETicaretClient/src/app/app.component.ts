c74d9f10658ab20b9b37f1b28fa96adbd3b3a0f7
Fırat IŞILDAK <firatisildak1@gmail.com>
2024-11-05 10:51:05 +0300

    jquary and bootstrap library added

--- ETicaretClient/src/app/app.component.ts
+++ ETicaretClient/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare var $: any
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ETicaretClient';
+  ngAfterViewInit() {
+    $(document).ready(() => {
+      alert("asd");
+    });
+  }
 }
