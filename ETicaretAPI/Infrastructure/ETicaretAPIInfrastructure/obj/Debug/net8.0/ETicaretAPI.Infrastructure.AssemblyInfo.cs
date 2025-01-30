5fb71c8db689ee37c3a7a16d680532e15c89a12c
Fırat IŞILDAK <firatisildak1@gmail.com>
2025-01-30 11:53:04 +0300

    file add algorithim finish

--- ETicaretAPI/Infrastructure/ETicaretAPIInfrastructure/obj/Debug/net8.0/ETicaretAPI.Infrastructure.AssemblyInfo.cs
+++ ETicaretAPI/Infrastructure/ETicaretAPIInfrastructure/obj/Debug/net8.0/ETicaretAPI.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ETicaretAPI.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dea3f65f9c39d4ba0c4174c3707b73569347145")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1031f394548cb7c41ab0c2736ab55d49ae7a172")]
 [assembly: System.Reflection.AssemblyProductAttribute("ETicaretAPI.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ETicaretAPI.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
