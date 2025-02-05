d415f1277973e7fefb166a5ec5da8286e402f7ca
Fırat IŞILDAK <firatisildak1@gmail.com>
2025-02-05 11:55:30 +0300

    mig_6 created

--- ETicaretAPI/Core/ETicaretAPI.Domain/Entities/Product.cs
+++ ETicaretAPI/Core/ETicaretAPI.Domain/Entities/Product.cs
@@ -1,18 +1,15 @@
 ﻿using ETicaretAPI.Domain.Entities.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ETicaretAPI.Domain.Entities
 {
-    public class Product:BaseEntity
+    public class Product : BaseEntity
     {
         public string Name { get; set; } = string.Empty;
-        public int Stock {  get; set; }
+        public int Stock { get; set; }
         public float Price { get; set; }
 
         public ICollection<Order> Orders { get; set; } = new List<Order>();
+
+        public ICollection<ProductImageFile> ProductImageFiles { get; set; }
     }
 }
--- ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/obj/Debug/net8.0/ETicaretAPI.Persistence.AssemblyInfo.cs
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/obj/Debug/net8.0/ETicaretAPI.Persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ETicaretAPI.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+279e34522068a40ab306a9577976b077055099d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5bcbdfa7a0bd1b5bfe61e7ba94abd9d07b7d3bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ETicaretAPI.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ETicaretAPI.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
